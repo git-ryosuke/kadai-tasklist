1f421c9fd53f907d891e61aee06c0553337fac19
ryosuke <git.ryosuke@gmail.com>
2019-02-09 08:23:42 +0000

    CRUD tasks Rev0.0

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <h1>たすく一覧</h1>
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>メッセージ</th>
+                    <th>たすく</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
+
+    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
