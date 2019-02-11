a87d2a172176fb6c521e7de516e6cebdd3a9b411
ryosuke <git.ryosuke@gmail.com>
2019-02-11 03:43:04 +0000

    add column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
         @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_tasklist')
+            
             @yield('content')
         </div>
         
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>たすく新規作成ページ</h1>
 
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'たすく:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('status', 'すてーたす:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
