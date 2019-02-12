4e1169647b77e5b12561384b4f7d0ae83bebc3cc
ryosuke <git.ryosuke@gmail.com>
2019-02-12 13:17:53 +0000

    registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the タスクりすと</h1>
+            {!! link_to_route('signup.get', '今すぐユーザ登録！', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
